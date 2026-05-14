47bb1373560ba10937286f156c421dcbf45e8948
Tabare8031 <facundovuerra@gmail.com>
2026-05-14 16:11:15 -0300

    Update EstacionDeCarga.java

--- src/main/java/TallerJakartaEE/ModuloDeCarga/Dominio/EstacionDeCarga.java
+++ src/main/java/TallerJakartaEE/ModuloDeCarga/Dominio/EstacionDeCarga.java
@@ -1,5 +1,56 @@
 package TallerJakartaEE.ModuloDeCarga.Dominio;
 
 public class EstacionDeCarga {
+    private String descripcion;
+    private String calle;
+    private String departamento;
+    private int longitud;
+    private int latitud;
 
+    public EstacionDeCarga(){
+
+    }
+    public EstacionDeCarga(String descripcion, String calle, String departamento, int longitud, int latitud){
+
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public int getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(int latitud) {
+        this.latitud = latitud;
+    }
 }
