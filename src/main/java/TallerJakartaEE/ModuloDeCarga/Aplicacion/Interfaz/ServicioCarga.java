60e4846b88c95f47908aec5a3de140ab7045dc92
Supraim <supraimofc@gmail.com>
2026-05-26 18:14:08 -0300

    Update ServicioCarga.java

--- src/main/java/TallerJakartaEE/ModuloDeCarga/Aplicacion/Interfaz/ServicioCarga.java
+++ src/main/java/TallerJakartaEE/ModuloDeCarga/Aplicacion/Interfaz/ServicioCarga.java
@@ -3,7 +3,6 @@ package TallerJakartaEE.ModuloDeCarga.Aplicacion.Interfaz;
 import TallerJakartaEE.ModuloDeCarga.Dominio.Carga;
 import TallerJakartaEE.ModuloDeCarga.Dominio.Cargador;
 import TallerJakartaEE.ModuloDeCarga.Dominio.EstacionDeCarga;
-import TallerJakartaEE.ModuloDeCarga.Interfaces.CargaResumenDTO;
 import TallerJakartaEE.ModuloDeClientes.Dominio.Cliente;
 import TallerJakartaEE.ModuloDePagos.Dominio.MedioDePago;
 
@@ -13,7 +12,7 @@ import java.util.List;
 public interface ServicioCarga {
     public void iniciarCarga(Cliente cliente, MedioDePago medioDePago);
     public Carga verCargaActual(Cliente cliente);
-    public List<CargaResumenDTO> verHistorico(Cliente cliente, LocalDateTime inicio, LocalDateTime fin);
+    public List<Carga> verHistorico(Cliente cliente, LocalDateTime inicio, LocalDateTime fin);
     public void finalizarCarga(Cargador cargador, Carga carga, LocalDateTime recargo);
     public void altaEstacion(EstacionDeCarga estacion);
     public void altaCargador(Cargador cargador);
