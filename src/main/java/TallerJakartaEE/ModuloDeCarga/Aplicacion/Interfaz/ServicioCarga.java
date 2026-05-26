ff68520e1ccd76d2a90a41ee60d4ede61e60e6ed
Supraim <supraimofc@gmail.com>
2026-05-26 15:36:59 -0300

    Create ServicioCarga.java
    Es una version basica con la firma de los metodos, hay que modificar los tipos de datos correspondientes

---
+++ src/main/java/TallerJakartaEE/ModuloDeCarga/Aplicacion/Interfaz/ServicioCarga.java
@@ -0,0 +1,21 @@
+package TallerJakartaEE.ModuloDeCarga.Aplicacion.Interfaz;
+
+import TallerJakartaEE.ModuloDeCarga.Dominio.Carga;
+import TallerJakartaEE.ModuloDeCarga.Dominio.Cargador;
+import TallerJakartaEE.ModuloDeCarga.Dominio.EstacionDeCarga;
+import TallerJakartaEE.ModuloDeCarga.Interfaces.CargaResumenDTO;
+import TallerJakartaEE.ModuloDeClientes.Dominio.Cliente;
+import TallerJakartaEE.ModuloDePagos.Dominio.MedioDePago;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ServicioCarga {
+    public void iniciarCarga(Cliente cliente, MedioDePago medioDePago);
+    public Carga verCargaActual(Cliente cliente);
+    public List<CargaResumenDTO> verHistorico(Cliente cliente, LocalDateTime inicio, LocalDateTime fin);
+    public void finalizarCarga(Cargador cargador, Carga carga, LocalDateTime recargo);
+    public void altaEstacion(EstacionDeCarga estacion);
+    public void altaCargador(Cargador cargador);
+    public List<EstacionDeCarga> obtenerEstaciones();
+}
